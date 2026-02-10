5729539bfa3cc31f64d367cbdaf583e676ac902a
mdedealf <dede.alfaruqq@gmail.com>
2026-02-10 22:39:30 +0700

    feat : adding listing card for home page

--- src/layouts/Container.tsx
+++ src/layouts/Container.tsx
@@ -5,7 +5,7 @@ interface ContainerProps {
 }
 
 const Container = ({ children }: ContainerProps) => {
-  return <div className="mt-18 lg:mt-24 px-4 md:px-20 py-2">{children}</div>;
+  return <div className="mt-20 lg:mt-26 px-4 md:px-20 py-2">{children}</div>;
 };
 
 export default Container;
